77c473d909b5a8ff925c338fb41f3677224bb942
jty <2849037341@qq.com>
2019-12-19 01:46:51 +0800

    配置gitpage

---
+++ precache-manifest.2f96fccf6e84aa12fa37f7c80cf1c64e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84594b4a8bcaa8672c9848860c4c7cef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17bb73a805b0eac2f438",
+    "url": "/static/css/main.eba0a30e.chunk.css"
+  },
+  {
+    "revision": "3a88cb01221a5341427b",
+    "url": "/static/js/2.de5526a5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.de5526a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17bb73a805b0eac2f438",
+    "url": "/static/js/main.ab5f46af.chunk.js"
+  },
+  {
+    "revision": "565014f5507082636f62",
+    "url": "/static/js/runtime-main.a1095af6.js"
+  }
+]);
